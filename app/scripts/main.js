51eb353b2183a1086e62ef9b63bed2ed3d7a5e78
Becca Aaronson <becca.aaronson@gmail.com>
2015-04-23 11:52:21 -0500

    add test jsons, test load script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+/* Main script file */
+
+$(document).ready(function() {
+
+  var data = [];
+
+  //Load JSON data from Google Spreadsheet
+  $.getJSON('/scripts/opp.json', function(json) {
+    data = json;
+  }).done( function() {
+    build(data);
+  });
+
+  function build(data) {
+
+    for (var i = 0; i < data.records.length; i++) {
+      var record = data.records[i];
+      console.log(data.records[i]);
+      $('<p>' + record.Account_Informal_Greeting__c + '</p>'
+        ).appendTo('.test');
+    }
+  }
+
+});

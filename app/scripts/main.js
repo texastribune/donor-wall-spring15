bf1234caeeb0737cf265018907c657849d37df11
Becca Aaronson <becca.aaronson@gmail.com>
2015-04-23 13:26:41 -0500

    add basic template, styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,19 +6,43 @@ $(document).ready(function() {
   var data = [];
 
   //Load JSON data from Google Spreadsheet
-  $.getJSON('/scripts/opp.json', function(json) {
+  $.getJSON('/scripts/account.json', function(json) {
     data = json;
   }).done( function() {
     build(data);
   });
 
-  function build(data) {
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
+  function build(data) {
     for (var i = 0; i < data.records.length; i++) {
       var record = data.records[i];
-      console.log(data.records[i]);
-      $('<p>' + record.Account_Informal_Greeting__c + '</p>'
-        ).appendTo('.test');
+      var name = '<span>' + record.Name + '</span>';
+      var amount = record.Total_Donor_Wall_This_Year__c;
+      // var span = '<span>' + name + ' $' + numberWithCommas(amount) + '</span>';
+      // console.log(span);
+
+      if (amount <= 10) {
+        $(name).appendTo('.student');
+      } else if (amount <= 35) {
+        $(name).appendTo('.enthusiast');
+      } else if (amount <= 60 ) {
+        $(name).appendTo('.activist');
+      } else if (amount <= 150 ) {
+        $(name).appendTo('.advocate');
+      } else if (amount <= 250 ) {
+        $(name).appendTo('.diplomat');
+      } else if (amount <= 500 ) {
+        $(name).appendTo('.benefactor');
+      } else {
+        $('<li>$' + numberWithCommas(amount) + ' &mdash; ' + name + '</li>').appendTo('.large-donors')
+      }
+
+      //
+      // at 1000, show individuals + amounts
+
     }
   }
 
